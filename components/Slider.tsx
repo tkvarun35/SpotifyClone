67c8f0c292cd68116745ebc4a9efb6255ff9c495
Varun <varun@ERPs-MacBook-Pro.local>
2023-09-28 00:53:23 +0530

    Added Slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -7,8 +7,48 @@ interface SliderProps {
   onChange?: (value: number) => void;
 }
 
-const Slider: React.FC<SliderProps> = ({ value, onChange }) => {
-  return <div>Slider</div>;
+const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
+  const handleChange = (newValue: number[]) => {
+    onChange?.(newValue[0]);
+  };
+
+  return (
+    <RadixSlider.Root
+      className="
+  relative
+  flex
+  items-center
+  select-none
+  touch-none
+  w-full
+  h-10
+"
+      defaultValue={[0]}
+      value={[value]}
+      onValueChange={handleChange}
+      max={1}
+      step={0.1}
+      aria-label="Volume"
+    >
+      <RadixSlider.Track
+        className="bg-neutral-600
+      relative
+      grow
+      rounded-full
+      h-[3px]
+      "
+      >
+        <RadixSlider.Range
+          className="
+          absolute
+        bg-white
+          rounded-full
+          h-full
+          "
+        />
+      </RadixSlider.Track>
+    </RadixSlider.Root>
+  );
 };
 
 export default Slider;
